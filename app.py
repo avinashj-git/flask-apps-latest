ceba4e586750c936992763ddc431beef5784a719
root <root@ip-172-31-46-152.eu-north-1.compute.internal>
2025-07-20 19:26:13 +0000

    all test cases passed! ready to deploy production

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Test automated deployment successfully completed and validated"
+    return "all test cases passed! Ready to deploy in production"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
