1259a35f018ea7a868e07099fe73d43d2260ab9d
root <root@ip-172-31-46-152.eu-north-1.compute.internal>
2025-07-20 19:57:22 +0000

    port commited in app.py dockerfile and script

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "all test cases passed! Ready to deploy in production"
+    return "all test cases passed! Ready to deploy in production and port also updated in the script for container"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
