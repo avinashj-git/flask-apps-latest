51a1df4d3977d74d8ab866a8e7b5c1aabe81c25b
root <root@ip-172-31-46-152.eu-north-1.compute.internal>
2025-07-20 19:20:01 +0000

    test automated deployment successsfully complated and validated

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Test automated deployment"
+    return "Test automated deployment successfully completed and validated"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
